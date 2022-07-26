86abbbd1c21e1f859d8c4aa9947c708cd04c21b4
MichitakaTsuda <67595708+MichitakaTsuda@users.noreply.github.com>
2022-07-26 13:34:01 +0900

    router-complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { BrowserRouter, Link } from "react-router-dom";
-
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page2 } from "./Page2";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Link>Home</Link>
-        <Home />
-        <Page1 />
-        <Page2 />
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
       </div>
+      <Router />
     </BrowserRouter>
   );
 }
--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,18 @@
+import { Link, Outlet, useNavigate } from "react-router-dom";
+
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  const navigate = useNavigate();
+  const onClickDetailA = () => navigate("/page1/detailA");
+
   return (
     <div>
       <h1>Page1です</h1>
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
+      <Link to="/page1/detailB">DetailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
+      <Outlet />
     </div>
   );
 };
